6015f51c9d46cd7f977ea79c2caaad46b5dca761
jtth3brick <jeshuagustafson@berkeley.edu>
2023-10-23 15:55:57 -0700

    added example bot class

---
+++ example_class.py
@@ -0,0 +1,9 @@
+class rps:
+    def __init__(self):
+        return '''None type'''
+
+    def play(self):
+        return '''string type'''
+
+    def update(self, adversary_move):
+        return '''None type'''
